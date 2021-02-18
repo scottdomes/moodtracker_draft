776d350c5541c2dafb6e976691bffaf1326afca7
Scott Domes <scottdomes@gmail.com>
2021-02-18 11:46:16 -0800

    Created footer

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-} from 'react-native';
-import Header from './src/components/Header'
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import Header from './src/components/Header';
 
 const App = () => {
   return (
-    <Header text="Home" />
+    <View style={styles.container}>
+      <Header text="Home" />
+      <View style={styles.footer}><Text>Footer</Text></View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  footer: {
+    backgroundColor: 'white',
+    height: 85,
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: -4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
 
+    elevation: 5,
+  },
 });
 
 export default App;
