089430cce5e01f677c7c99ed55e146743df92c3a
Scott Domes <scottdomes@gmail.com>
2021-02-18 12:11:18 -0800

    Click icon

--- src/components/NavigationIcon.js
+++ src/components/NavigationIcon.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {grey, darkPurple} from '../styles/colors';
 
-const NavigationIcon = ({icon, isActive}) => {
+const NavigationIcon = ({icon, isActive, onPress}) => {
   const currentStyles = isActive
     ? {...styles.base, ...styles.active}
     : styles.base;
-  return <FontAwesomeIcon icon={icon} size={24} style={currentStyles} />;
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <FontAwesomeIcon icon={icon} size={24} style={currentStyles} />
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
