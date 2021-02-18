7b94926128b8de900c412d61c20765f7b8cbcd00
Scott Domes <scottdomes@gmail.com>
2021-02-18 12:25:44 -0800

    Add stack navigator

--- App.js
+++ App.js
@@ -3,17 +3,37 @@ import {StyleSheet, View, Text} from 'react-native';
 import Header from './src/components/Header';
 import NavigationIcon from './src/components/NavigationIcon';
 import {faHome, faPoll, faUser} from '@fortawesome/free-solid-svg-icons';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const App = () => {
+const Layout = ({screenName}) => {
   return (
     <View style={styles.container}>
-      <Header text="Home" />
+      <Header text={screenName} />
       <View style={styles.footer}>
         <NavigationIcon icon={faHome} isActive />
         <NavigationIcon icon={faPoll} />
         <NavigationIcon icon={faUser} />
       </View>
     </View>
+  );
+};
+
+const HomeScreen = () => <Layout screenName="Home" />;
+const TrendsScreen = () => <Layout screenName="Trends" />;
+const ProfileScreen = () => <Layout screenName="Profile" />;
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Trends" component={TrendsScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
