ca0e5aa26e9ae6ea0ff4f3d4d6e16fc1428c5ae3
Scott Domes <scottdomes@gmail.com>
2021-02-18 11:37:25 -0800

    Created header

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import {
+  StyleSheet,
+  ImageBackground,
+  Text,
+  View,
+} from 'react-native';
 
 const App = () => {
   return (
-    <>
-    </>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('./src/img/background.png')}
+        style={styles.image}>
+        <View >
+          <Text  style={styles.text}>Home</Text>
+        </View>
+      </ImageBackground>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    height: 250,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    paddingTop: 100
+  },
+  text: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 25
+  }
 });
 
 export default App;
