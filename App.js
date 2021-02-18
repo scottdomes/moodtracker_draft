39e6e971160ed090733b1ea657a664212ded33a9
Scott Domes <scottdomes@gmail.com>
2021-02-18 11:28:14 -0800

    Starting from scratch

--- App.js
+++ App.js
@@ -1,27 +1,15 @@
 import React, {useState} from 'react';
-import {Text, StyleSheet, View, Button} from 'react-native';
+import {StyleSheet} from 'react-native';
 
 const App = () => {
-  const [user, setUser] = useState(null)
   return (
     <>
-      <View style={styles.scrollView}>
-        <Text>Login status: {user ? user.email : 'Logged out'}</Text>
-        <Button title="Log in" />
-        <Button title="Sign up" />
-        <Button title="Log out" />
-      </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+
 });
 
 export default App;
