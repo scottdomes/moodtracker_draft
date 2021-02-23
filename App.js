f21d3994766021756d1ae9b8deb6a6f328590885
Scott Domes <scottdomes@gmail.com>
2021-02-22 17:07:27 -0800

    Added auth logic

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import auth from '@react-native-firebase/auth';
+
 import {SCREENS} from './src/constants';
 import AuthScreenContainer from './src/screens/AuthScreenContainer';
 import MainScreenContainer from './src/screens/MainScreenContainer';
 
 const App = () => {
   const [currentScreen, setScreen] = useState(SCREENS.SIGNUP);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user) {
+    setUser(user);
+    console.log(user);
+    if (user) setScreen(SCREENS.HOME);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
 
   if (
     currentScreen === SCREENS.LOGIN ||
