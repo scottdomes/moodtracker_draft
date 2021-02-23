91810dedc0ff2a989e7514da7623d46fab892e48
Scott Domes <scottdomes@gmail.com>
2021-02-22 17:29:09 -0800

    Forgot password screen

--- src/screens/Forgot.js
+++ src/screens/Forgot.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {Image, StyleSheet, View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
+
 import BasicButton from '../components/Button';
 import Card from '../components/Card';
 import Input from '../components/Input';
@@ -7,14 +9,35 @@ import {lightPurple} from '../styles/colors';
 import Link from '../components/Link';
 
 const Forgot = ({navigateToLogin}) => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+
+  const forgotPassword = () => {
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        navigateToLogin();
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
   return (
     <Card containerStyles={styles.card}>
       <View style={styles.center}>
         <Image source={require('../img/icon.png')} />
+        <Text style={styles.error}>{error}</Text>
       </View>
-      <Input label="Email" />
+      <Input
+        label="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        textContentType="emailAddress"
+      />
       <View style={styles.center}>
-        <BasicButton backgroundColor={lightPurple}>Submit</BasicButton>
+        <BasicButton backgroundColor={lightPurple} onPress={forgotPassword}>
+          Submit
+        </BasicButton>
         <View style={styles.linkContainer}>
           <Link onNavigate={navigateToLogin}>Back to login</Link>
         </View>
